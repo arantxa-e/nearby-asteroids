0d1b4824ca94da48f2d058f287ac4788192d2a40
Arantxa Edwards <aedwardsglass@gmail.com>
2019-10-26 16:54:01 -0400

    stuck on how to parse through nested json objects

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// const Data = ({data, day}) => {
+//     let objects = data.near_earth_objects
+//     console.log(objects.data)
+//     return(
+//         <div>
+//             logged
+//         </div>
+//     )
+// }
+// I'm stuck here
+
+
 class Nasa extends React.Component {
     state = {
         data: [],
         loading: false,
-        start: '',
-        end: '',
+        day: '',
         error: false
     }
 
@@ -14,37 +25,19 @@ class Nasa extends React.Component {
         console.log(this.state)
     }
 
-    collectStartDate = (e) => {
-        this.setState({start: e.target.value})
-
-        let date = new Date(e.target.value)
-
-        // adding seven days to the start date
-        let plusSeven = new Date(date.setDate(date.getDate() + 8))
-
-        // adding 1 to the new month since count starts at 00
-        let newMonth = plusSeven.getMonth() + 1 
-        let newDate = plusSeven.getDate()
-        let newYear = plusSeven.getFullYear()
-
-        // converting month and date to mm and dd format
-        if (newMonth < 10) newMonth = `0${newMonth}`
-        if (newDate < 10) newDate = `0${newDate}`
-        
-        this.setState({end: `${newYear}-${newMonth}-${newDate}`})
+    collectDay = (e) => {
+        // setting the start date in the state
+        this.setState({day: e.target.value})
     }
-    
-
 
     outputDates = (e) => {
         e.preventDefault()
-        let start = this.state.start
-        let end = this.state.end
+        let day = this.state.day
         let key = 'uglaVaRE5v6sq7B6x7tskfi7GPjYpIIfwyH3w90u'
+        this.setState({loading: true, error: false})
 
-        if (start && end) {
-            this.setState({loading: true, error: false})
-            fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${start}&end_date=${end}&api_key=${key}`)
+        if (day) {
+            fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${day}&end_date=${day}&api_key=${key}`)
             .then(response => {
                 if (response.ok) {
                     return response.json()
@@ -58,37 +51,29 @@ class Nasa extends React.Component {
         } else {
             this.setState({error: true})
         }
-        
     }
     
-
-
     render() {
         return (
             <div>
                 <form onSubmit={this.outputDates}>
-                    <h1>Nasa Space Stuff goes here</h1>
-
-                    {(this.state.error ? 'Please select a start and end date then try again' : 'Successful')}
-
+                    <h1>Nasa Space Stuff</h1>
+                    Loading: {(this.state.error ? 'Loading...' : 'Done')}
                     <br />
-
+                    Error: {(this.state.error ? 'Please select a start and end date then try again' : 'Successful')}
+                    <br />
                     <label>
-                        From
-                        <input type="date" id="from-date" onChange={this.collectStartDate}></input>
+                        Day
+                        <br />
+                        <input type="date" onChange={this.collectDay}></input>
                     </label>
-    
                     <br />
-    
-                    <label>
-                        To
-                        
-                    </label>
-    
-                    <br />
-    
                     <button type="submit">Submit</button>
                 </form>
+
+                <div>
+                    {this.state.data ? <Data data={this.state.data} day={this.state.day} /> : 'awaiting data' }
+                </div>
             </div>
         )
     }
