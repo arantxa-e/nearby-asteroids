0d9a324868570239d7dca4df81013e2abbdcb370
Arantxa Edwards <aedwardsglass@gmail.com>
2019-10-26 13:39:45 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Nasa extends React.Component {
+    state = {
+        data: [],
+        loading: false
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    outputDates = (e) => {
+        e.preventDefault()
+        this.setState({loading: true})
+        fetch('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=uglaVaRE5v6sq7B6x7tskfi7GPjYpIIfwyH3w90u')
+            .then(response => (
+                response.ok 
+                    ? this.setState({data: response.json()}) 
+                    : new Error(response.statusText)
+                ))
+            .then(
+                console.log(this.state.data),
+                this.setState({loading: false})
+            )
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.outputDates}>
+                    <h1>Nasa Space Stuff goes here</h1>
+                    {(this.state.loading ? 'loading...' : 'finished loading...')}
+                    <label>
+                        From
+                        <input type="date" id="from-date"></input>
+                    </label>
+    
+                    <br />
+    
+                    <label>
+                        To
+                        <input type="date" id="to-date"></input>
+                    </label>
+    
+                    <br />
+    
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Nasa />, document.getElementById('root'));
