36912b7686bbf8f8116b390837cea8abded38e8b
Arantxa Edwards <aedwardsglass@gmail.com>
2019-10-26 19:24:58 -0400

    Fixed fetching and passing data to Data component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const Data = ({data, day}) => {
-//     let objects = data.near_earth_objects
-//     console.log(objects.data)
-//     return(
-//         <div>
-//             logged
-//         </div>
-//     )
-// }
-// I'm stuck here
+const Data = ({num, name, url, feetMin, feetMax, hazard, approach, velocity}) => {
+    return(
+        <div>
+            <h1>{num + 1}. Name: <a href={url} target="_blank" rel="noopener noreferrer">{name}</a></h1>
+            <p>
+                Diameter: {Math.round(feetMin)} feet - {Math.round(feetMax)} feet 
+                <br />
+                Hazard level: {hazard ? 'Hazardous' : 'Not hazardous'}
+                <br />
+                Approach date: {approach}
+                <br />
+                Velocity: {Math.round(velocity)} miles per hour
+                
+
+            </p>
+        </div>
+    )
+}
+
 
 
 class Nasa extends React.Component {
@@ -38,16 +47,16 @@ class Nasa extends React.Component {
 
         if (day) {
             fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${day}&end_date=${day}&api_key=${key}`)
-            .then(response => {
-                if (response.ok) {
-                    return response.json()
-                } else {
-                    throw new Error(response.statusText)
-                }
-            })
-            .then(response => this.setState({data: response}))
-            .then(this.setState({loading: false}))
-            .catch(error => console.log(error))
+                .then(response => {
+                    if (response.ok) {
+                        return response.json()
+                    } else {
+                        throw new Error(response.statusText)
+                    }
+                })
+                .then(response => this.setState({data: response.near_earth_objects[day]}))
+                .then(this.setState({loading: false}))
+                .catch(error => console.log(error))
         } else {
             this.setState({error: true})
         }
@@ -72,7 +81,21 @@ class Nasa extends React.Component {
                 </form>
 
                 <div>
-                    {this.state.data ? <Data data={this.state.data} day={this.state.day} /> : 'awaiting data' }
+                    {this.state.data.map(
+                        (data, i) => 
+                            <Data 
+                                key={i}
+                                num={i}
+                                name={data.name} 
+                                url={data.nasa_jpl_url}
+                                feetMin={data.estimated_diameter.feet.estimated_diameter_min}
+                                feetMax={data.estimated_diameter.feet.estimated_diameter_max}
+                                hazard={data.is_potentially_hazardous_asteroid}
+                                approach={data.close_approach_data[0].close_approach_date}
+                                velocity={data.close_approach_data[0].relative_velocity.miles_per_hour}
+
+                            />
+                    )}
                 </div>
             </div>
         )
